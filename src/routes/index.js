64626a7a6d12a939105e04ee6c7cd6b0e2c89e53
pengin7384 <pengin7384@gmail.com>
2020-12-27 00:40:27 +0900

    Change health path to '/'

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = new express.Router();
 const apiV1Router = require('./api/v1');
 
-router.use('/health', (req, res) => {
-  res.send('OK');
-});
 
 router.use('/api/v1', apiV1Router);
+
+router.get('/', (req, res) => {
+  res.send('OK');
+});
 
 module.exports = router;
