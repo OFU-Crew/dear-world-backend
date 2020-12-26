c75b3e25ceb3a4250efe4f93f03e06b0f6750408
kor-Chipmunk <rhj4862@gmail.com>
2020-12-27 02:02:22 +0900

    Add getMessages mock api

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,6 +1,8 @@
 // const messageService = require('../services/message.service');
 const multipleService = require('../services/multiple.service');
 const Response = require('../utils/response');
+const faker = require('faker');
+const {flag, name} = require('country-emoji');
 
 // TODO: Implement addMessage
 async function addMessage(req, res, next) {
@@ -27,6 +29,59 @@ async function addMessage(req, res, next) {
   }
 }
 
+async function getMessages(req, res, next) {
+  const {
+    countryId,
+    offset,
+  } = req.query;
+
+  if (!countryId || !offset) {
+    res.status(422).json(Response(-1, 'INVALID_REQUEST_PARAMETERS'));
+    return;
+  }
+
+  const mockMessages = [];
+  const allowedEmoji = '😊🙃🤪🤓🤯😴💩👻👽🤖👾👐🖖✌️🤟🤘🤙👋🐭🦕🦖🐉';
+
+  for (let i = 0; i < 1000; ++i) {
+    const countryCode = faker.address.countryCode();
+    const countryImage = flag(countryCode);
+    const countryFullName = name(countryImage);
+
+    mockMessages.push({
+      'uuid': faker.random.uuid(),
+      'anonymousUser': {
+        'id': i,
+        'country': {
+          'shortName': countryCode,
+          'fullName': countryFullName,
+          'image': countryImage,
+        },
+        'nickname': faker.name.findName(),
+        'profileImage': allowedEmoji[
+            Math.floor(
+                Math.random() * allowedEmoji.length,
+            )
+        ],
+      },
+      'content': faker.lorem.paragraphs(Math.floor(Math.random() * 8) + 1),
+      'likeCount': faker.random.number(10000),
+      'createdAt': faker.date.between(
+          new Date('2020-12-01T00:00:00'),
+          new Date('2020-12-31T00:00:00'),
+      ),
+      'like': faker.random.boolean(),
+    });
+  }
+
+  res.status(200).json(Response(1, 'SUCCESS_GET_MESSAGE_LIST', {
+    'firstId': mockMessages[offset || 0].uuid,
+    'lastId': mockMessages[offset + 29 || 29].uuid,
+    'messages': mockMessages,
+  }));
+}
+
 module.exports = {
   addMessage,
+  getMessages,
 };
