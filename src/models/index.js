753820c26838e28673ebb119c4ba3129c5709866
pengin7384 <pengin7384@gmail.com>
2020-12-26 21:44:23 +0900

    Add models index.js

---
+++ src/models/index.js
@@ -0,0 +1,41 @@
+const { Sequelize } = require('sequelize');
+const config = require('../configs/mysql_config');
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+  }
+);
+const db = {
+  sequelize,
+  Sequelize,
+};
+
+db.Country = require('./country')(sequelize);
+db.CountryStatus = require('./country_status')(sequelize);
+db.AnonymousUser = require('./anonymous_user')(sequelize);
+db.Message = require('./message')(sequelize);
+db.LikeHistory = require('./like_history')(sequelize);
+db.Emoji = require('./emoji')(sequelize);
+
+// Associations
+db.CountryStatus.hasOne(db.Country, {
+  foreignKey: 'country_id',
+});
+db.AnonymousUser.belongsTo(db.Country, {
+  foreignKey: 'country_id',
+});
+db.AnonymousUser.belongsTo(db.Emoji, {
+  foreignKey: 'emoji_id',
+});
+db.Message.belongsTo(db.AnonymousUser, {
+  foreignKey: 'anonymous_user_id',
+});
+db.LikeHistory.belongsTo(db.Message, {
+  foreignKey: 'message_id',
+});
+
+module.exports = db;
