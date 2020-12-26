5922ef7940e1fdc175ea42405e920264d0782a71
pengin7384 <pengin7384@gmail.com>
2020-12-27 04:11:55 +0900

    Add emojiRouter & countryRouter

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = new express.Router();
 const messageRouter = require('./message.route');
+const emojiRouter = require('./emoji.route');
+const countryRouter = require('./country.route');
 
 router.use('/messages', messageRouter);
+router.use('/emojis', emojiRouter);
+router.use('/countries', countryRouter);
 
 module.exports = router;
