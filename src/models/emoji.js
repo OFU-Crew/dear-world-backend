518bbcce9297c4b2ad567d07b33a31cbb109ad03
pengin7384 <pengin7384@gmail.com>
2020-12-26 21:47:24 +0900

    Change table name

--- src/models/emoji.js
+++ src/models/emoji.js
@@ -1,7 +1,7 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  const Emoji = sequelize.define('LikeHistory', {
+  const Emoji = sequelize.define('Emoji', {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
