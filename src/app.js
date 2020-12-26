8171856ab7f7cf812af2823d2a9153c0dab9f36d
kor-Chipmunk <rhj4862@gmail.com>
2020-12-26 23:13:20 +0900

    Add express rate limit middleware

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const router = require('./routes');
+const limiter = require('./middlewares/express-rate-limit');
 
+app.use(limiter);
 app.use(express.json());
 app.use(router);
 app.use((req, res, next) => {
