2d660c3d1f1ef9a1ffb0f612bc6a9fde4a8090d0
kor-Chipmunk <rhj4862@gmail.com>
2020-12-27 02:47:58 +0900

    Fix npx pm2 reinstall script, mock api language, cors allow

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ echo '============================'
 echo 'Running install_dependencies'
 echo '============================'
 
-/usr/local/bin/pm2 kill
+npx pm2 kill
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/dist/dear-world-production/
 npm install
