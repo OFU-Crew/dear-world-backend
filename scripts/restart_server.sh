ec6feefa64391f6525a6fbeda598943b9a39ab39
kor-Chipmunk <rhj4862@gmail.com>
2020-12-27 18:18:15 +0900

    Add script code

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -3,4 +3,10 @@ echo '======================'
 echo 'Running restart_server'
 echo '======================'
 
+<<<<<<< HEAD
 NODE_ENV=production npx cross-env pm2 start /home/ec2-user/app/dist/dear-world-production/src/app.js -i 0 --name "admin" &
+=======
+NODE_ENV=production npx pm2 start /home/ec2-user/app/dist/dear-world-production/src/app.js -i 0 --name "admin" &
+NODE_ENV=production npx nodemon /home/ec2-user/app/dist/dear-world-production/src/scripts/init_countries_table.js
+NODE_ENV=production npx nodemon /home/ec2-user/app/dist/dear-world-production/src/scripts/init_emojies_table.js
+>>>>>>> Add script code
