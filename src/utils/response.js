8adfd495eb6f9f9474e8220d062d7fcc67e894cd
pengin7384 <pengin7384@gmail.com>
2020-12-27 18:18:22 +0900

    Change response

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,7 +1,20 @@
-module.exports = (code, detailCode, data = null) => {
+const Response = (code, message, data) => {
   return {
     code: code,
-    detailCode: detailCode,
-    data,
+    message: message,
+    data: data,
   };
+};
+
+const Success = (data) => {
+  return Response(1, null, data);
+};
+
+const Failure = (message) => {
+  return Response(0, message, null);
+};
+
+module.exports = {
+  Success,
+  Failure,
 };
