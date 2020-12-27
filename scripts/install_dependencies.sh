7306692e15aaad510d64f83890c30d97ec92e188
kor-Chipmunk <rhj4862@gmail.com>
2020-12-27 19:19:02 +0900

    Fix script code

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,10 +3,7 @@ echo '============================'
 echo 'Running install_dependencies'
 echo '============================'
 
-npx pm2 kill
-source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/dist/dear-world-production/
+source /home/ec2-user/.bash_profile
+npx pm2 kill
 npm install
-
-NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_countries_table.js
-NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_emojies_table.js
--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -3,6 +3,6 @@ echo '======================'
 echo 'Running restart_server'
 echo '======================'
 
-NODE_ENV=production npx pm2 start /home/ec2-user/app/dist/dear-world-production/src/app.js -i 0 --name "admin" &
-NODE_ENV=production npx nodemon /home/ec2-user/app/dist/dear-world-production/src/scripts/init_countries_table.js
-NODE_ENV=production npx nodemon /home/ec2-user/app/dist/dear-world-production/src/scripts/init_emojies_table.js
+cd /home/ec2-user/app/dist/dear-world-production/
+source /home/ec2-user/.bash_profile
+NODE_ENV=production npx pm2 start src/app.js -i 0 --name "admin" &
