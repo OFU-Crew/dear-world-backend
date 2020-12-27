0c4279257a8dd5ce2fd94eff8caad4c5991a03f9
kor-Chipmunk <rhj4862@gmail.com>
2020-12-27 18:54:59 +0900

    Fix seeder script

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -3,4 +3,6 @@ echo '======================'
 echo 'Running restart_server'
 echo '======================'
 
-NODE_ENV=production npx pm2 start /home/ec2-user/app/dist/dear-world-production/src/app.js -i 0 --name "admin" &
+NODE_ENV=production npx cross-env pm2 start /home/ec2-user/app/dist/dear-world-production/src/app.js -i 0 --name "admin" &
+NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_countries_table.js
+NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_emojies_table.js
---
+++ src/scripts/init_emojies_table.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({path: path.join(__dirname, '../../.env.production')});
+} else if (process.env.NODE_ENV === 'develop' ||
+              process.env.NODE_ENV === undefined) {
+  dotenv.config({path: path.join(__dirname, '../../.env.develop')});
+} else {
+  throw new Error('process.env.NODE_ENV를 올바르게 설정해주세요!');
+}
+
+const dataList = [
+  '1F970',
+  '1F60D',
+  '1F917',
+  '1F92F',
+  '1F973',
+  '1F496',
+  '1F647',
+  '1F643',
+  '1F6CC',
+  '1F6C0',
+  '1F42F',
+  '1F340',
+  '1F37F',
+  '1F957',
+  '1F37B',
+  '1F3D6',
+  '1F305',
+  '1F381',
+  '1F4EE',
+  '1F4C6',
+  '1F47D',
+];
+
+const emojiService = require('../services/emoji.service');
+async function main() {
+  for (const data of dataList) {
+    try {
+      await emojiService.addEmoji(String.fromCodePoint(parseInt(data, 16)));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+main();
