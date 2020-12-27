77fe2c202ea9a601ccb0c4fd19a1d462c5b614b5
kor-Chipmunk <rhj4862@gmail.com>
2020-12-27 19:02:42 +0900

    Fix seed script move to install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,3 +7,6 @@ npx pm2 kill
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/dist/dear-world-production/
 npm install
+
+NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_countries_table.js
+NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_emojies_table.js
--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -5,5 +5,3 @@ echo '======================'
 
 cd /home/ec2-user/app/dist/dear-world-production/
 NODE_ENV=production npx cross-env pm2 start /home/ec2-user/app/dist/dear-world-production/src/app.js -i 0 --name "admin" &
-NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_countries_table.js
-NODE_ENV=production npx cross-env npx node /home/ec2-user/app/dist/dear-world-production/src/scripts/init_emojies_table.js
