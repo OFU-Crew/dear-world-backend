fdcfb54edbae59419e6c6189e32a0981f1a0420a
pengin7384 <pengin7384@gmail.com>
2021-01-03 04:35:55 +0900

    Delete mock codes & Fix upperCase issue

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const messageService = require('../services/message.service');
 const multipleService = require('../services/multiple.service');
 const {Success, Failure} = require('../utils/response');
@@ -18,7 +19,7 @@ async function addMessage(req, res, next) {
 
   try {
     const result = await multipleService.addAnonymousUserAndMessage(
-        content, countryCode.toUpperCase(), emojiId, nickname,
+        content, _.upperCase(countryCode), emojiId, nickname,
     );
 
     res.status(200).json(Success(result));
@@ -40,7 +41,7 @@ async function getMessage(req, res, next) {
     const result = await messageService.getMessage(
         ipv4,
         messageId,
-        countryCode.toUpperCase(),
+        _.upperCase(countryCode),
         position,
     );
     res.status(200).json(Success(result));
@@ -58,71 +59,12 @@ async function getMessages(req, res, next) {
 
   const ipv4 = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   try {
-    const result = await messageService.getMessages(ipv4, countryCode, type,
-        lastId);
+    const result = await messageService.getMessages(ipv4,
+        _.upperCase(countryCode), type, lastId);
     res.status(200).json(Success(result));
   } catch (err) {
     res.status(200).json(Failure(err.message));
   }
-
-  const mockMessages = [];
-  const allowedEmoji = [...'😊🙃🤪🤓🤯😴💩👻👽🤖👾👐🖖✌️🤟🤘🤙👋🐭🦕🦖🐉'];
-
-  for (let i = 0; i < 1000; ++i) {
-    let countryCode = faker.address.countryCode();
-    if (i % 20 <= 8) {
-      countryCode = 'kr';
-    } else if (i % 20 <= 12) {
-      countryCode = 'jp';
-    } else if (i % 20 <= 16) {
-      countryCode = 'zh_CN';
-    }
-
-    const countryImage = flag(countryCode);
-    const countryFullName = name(countryImage);
-
-    let locale = 'en';
-    if (countryCode === 'kr') {
-      locale = 'ko';
-    } else if (countryCode === 'jp') {
-      locale = 'ja';
-    } else if (countryCode === 'cn') {
-      locale = 'zh_CN';
-    }
-
-    faker.locale = locale;
-
-    mockMessages.push({
-      'uuid': faker.random.uuid(),
-      'anonymousUser': {
-        'id': i,
-        'country': {
-          'shortName': countryCode.toUpperCase(),
-          'fullName': countryFullName,
-          'image': countryImage,
-        },
-        'nickname': allowedEmoji[
-            Math.floor(
-                Math.random() * allowedEmoji.length,
-            )
-        ],
-        'profileImage': faker.image.imageUrl(200, 200),
-      },
-      'content': faker.lorem.paragraphs(Math.floor(Math.random() * 8) + 1),
-      'likeCount': faker.random.number(10000),
-      'createdAt': faker.date.between(
-          new Date('2020-12-01T00:00:00'),
-          new Date('2020-12-31T00:00:00'),
-      ),
-      'like': faker.random.boolean(),
-    });
-  }
-
-  res.status(200).json(Success({
-    'firstId': mockMessages[Number(offset)].uuid,
-    'lastId': mockMessages[Number(offset) + 29].uuid,
-    'messages': mockMessages.slice(Number(offset), Number(offset)+30),
-  }));
 }
 
 async function postLikeMessage(req, res, next) {
