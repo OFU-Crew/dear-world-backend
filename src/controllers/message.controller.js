6820ab1f6d3d16a4087668ef42679126f9b41c5b
pengin7384 <pengin7384@gmail.com>
2021-01-03 19:44:44 +0900

    Add shareLink to message

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -87,22 +87,9 @@ async function postLikeMessage(req, res, next) {
   }
 }
 
-async function getShareLink(req, res, next) {
-  const {messageId} = req.params;
-  try {
-    const shareLink = messageService.getShareLink(messageId);
-    res.status(200).json(Success({
-      shareLink,
-    }));
-  } catch (err) {
-    res.status(200).json(Failure(err.message));
-  }
-}
-
 module.exports = {
   addMessage,
   getMessage,
   getMessages,
   postLikeMessage,
-  getShareLink,
 };
