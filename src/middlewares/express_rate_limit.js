1973f0a95e4e11964e9d42b4e9d1ab786c63c45d
kor-Chipmunk <rhj4862@gmail.com>
2021-01-05 00:22:56 +0900

    Add rate-limit-redis expiry added

--- src/middlewares/express_rate_limit.js
+++ src/middlewares/express_rate_limit.js
@@ -1,6 +1,12 @@
 const rateLimit = require('express-rate-limit');
+const RedisStore = require('rate-limit-redis');
+const {redisDefault} = require('../redis');
 
 const limiter = rateLimit({
+  store: new RedisStore({
+    client: redisDefault,
+    expiry: 300,
+  }),
   windowMs: 1000,
   max: 10,
 });
