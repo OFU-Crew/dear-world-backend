a333bbb170cc1dd597a72cb525cd7a902907f976
kor-Chipmunk <rhj4862@gmail.com>
2021-01-05 02:18:48 +0900

    Fix host

--- src/redis.js
+++ src/redis.js
@@ -1,8 +1,8 @@
 const redis = require('redis');
 const {promisify} = require('util');
 
-const redisDefault = redis.createClient(host=process.env.REDIS_DEFAULT);
-const redisReadonly = redis.createClient(host=process.env.REDIS_READONLY);
+const redisDefault = redis.createClient(process.env.REDIS_DEFAULT);
+const redisReadonly = redis.createClient(process.env.REDIS_READONLY);
 
 const getAsyncDefault = promisify(redisDefault.get).bind(redisDefault);
 const getAsyncReadonly = promisify(redisReadonly.get).bind(redisReadonly);
