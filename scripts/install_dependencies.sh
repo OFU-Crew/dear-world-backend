3d642da920a268a7683d942a5de2bdf3e220fea1
kor-Chipmunk <rhj4862@gmail.com>
2021-01-05 01:59:19 +0900

    Fix script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,5 @@ echo 'Running install_dependencies'
 echo '============================'
 
 cd /home/ec2-user/app/dist/dear-world-production/
+source /home/ec2-user/.bash_profile
 npm install
--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -4,4 +4,5 @@ echo 'Running restart_server'
 echo '======================'
 
 cd /home/ec2-user/app/dist/dear-world-production/
+source /home/ec2-user/.bash_profile
 npx pm2 reload ecosystem.config.js --env production &
