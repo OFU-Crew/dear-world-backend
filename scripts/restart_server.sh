c335080cea86228f98e745cabe915847f3b397af
kor-Chipmunk <rhj4862@gmail.com>
2021-01-05 00:53:10 +0900

    Fix restart server script

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -5,4 +5,4 @@ echo '======================'
 
 cd /home/ec2-user/app/dist/dear-world-production/
 source /home/ec2-user/.bash_profile
-NODE_ENV=production npx pm2 start ecosystem.config.js -i 0 --name "admin" &
+NODE_ENV=production npx pm2 reload ecosystem.config.js -i 0 --name "admin" &
