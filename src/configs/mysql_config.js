eb892122b07b5ed84e31bf2333180dab6fd48c43
kor-Chipmunk <rhj4862@gmail.com>
2021-01-09 18:11:37 +0900

    Add mysql_config env

--- src/configs/mysql_config.js
+++ src/configs/mysql_config.js
@@ -1,3 +1,17 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({path: path.join(__dirname, '../../.env.production')});
+} else if (process.env.NODE_ENV === 'test') {
+  dotenv.config({path: path.join(__dirname, '../../.env.test')});
+} else if (process.env.NODE_ENV === 'develop' ||
+              process.env.NODE_ENV === undefined) {
+  dotenv.config({path: path.join(__dirname, '../../.env.develop')});
+} else {
+  throw new Error('process.env.NODE_ENV를 올바르게 설정해주세요!');
+}
+
 module.exports = {
   host: process.env.MYSQL_HOST || 'localhost',
   database: process.env.MYSQL_DATABASE || 'dear_world',
